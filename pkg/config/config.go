b1802480d0038aa57506cfdba9aff1e4ec15f15f
Tom Bell <tomb@tomb.io>
2022-04-28 21:07:57 +0100

    Add config package
    Used to read the TOML configuration file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,7 +6,52 @@ import (
 	"github.com/pelletier/go-toml"
 )
 
-type Config struct{}
+type Config struct {
+	Directories []string `toml:"directories"`
+
+	Backup string `toml:"backup"`
+
+	Metadata struct {
+		Names struct {
+			Monday    string `toml:"monday"`
+			Tuesday   string `toml:"tuesday"`
+			Wednesday string `toml:"wednesday"`
+			Thursday  string `toml:"thursday"`
+			Friday    string `toml:"friday"`
+			Saturday  string `toml:"saturday"`
+			Sunday    string `toml:"sunday"`
+		} `toml:"names"`
+
+		Pictures struct {
+			Monday    string `toml:"monday"`
+			Tuesday   string `toml:"tuesday"`
+			Wednesday string `toml:"wednesday"`
+			Thursday  string `toml:"thursday"`
+			Friday    string `toml:"friday"`
+			Saturday  string `toml:"saturday"`
+			Sunday    string `toml:"sunday"`
+		} `toml:"pictures"`
+	} `toml:"metadata"`
+
+	GitHub struct {
+		Token string `toml:"token"`
+
+		Repo struct {
+			Owner  string `toml:"owner"`
+			Name   string `toml:"name"`
+			Branch string `toml:"branch"`
+		} `toml:"repo"`
+
+		Committer struct {
+			Name  string `toml:"name"`
+			Email string `toml:"email"`
+		} `toml:"committer"`
+	} `toml:"github"`
+
+	Mixcloud struct {
+		Token string `toml:"token"`
+	} `toml:"mixcloud"`
+}
 
 func Load(path string) (*Config, error) {
 	tree, err := toml.LoadFile(path)
